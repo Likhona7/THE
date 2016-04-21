c5cad8d38b37871daf8020cfad635508e451bdf4
likhona <likhonapr@projectcodex.co>
2016-04-21 17:12:28 +0200

    latest

--- bestDeal.js
+++ bestDeal.js
@@ -1,10 +1,48 @@
-module.exports = function(arr) {
+exports.Arr = function(arr) {
 
 var splitpric = arr.replace(/ for R/g, " ");
 
 var avoArr = splitpric.split(", ");
 
-console.log(avoArr);
 
+
+
+console.log(avoArr);
+//console.log(newArrAvo);
+return avoArr;
 
 }
+ exports.avoObj = function(avoArr){
+   var avoDealArr =[];
+   avoArr.forEach(function(arr){
+     var rows = arr.split(" ");
+    //  var quantity = rows[0];
+    //  var price = rows[1];
+
+     dealObj = {
+
+       price: rows[1],
+       quantity: rows[0]
+     }
+     avoDealArr.push(dealObj);
+
+   })
+   console.log(avoDealArr);
+   return avoDealArr;
+ }
+
+ exports.newArrAvo = function(avoDealArr) {
+ var newArrAvo = [];
+avoDealArr.forEach(function(arr){
+
+//console.log(arr);
+var pricePerAvo = 0;
+var pricePerAvo = Number(pricePerAvo + arr.price / arr.quantity).toFixed(2);
+console.log(arr);
+console.log(pricePerAvo);
+
+
+
+})
+//console.log(arr);
+ }
--- test/test_THE-BEST-OF-DEALS-2.js
+++ test/test_THE-BEST-OF-DEALS-2.js
@@ -3,15 +3,42 @@ var assert = require("assert");
 
 
 
-var strAvo = "1 for R3, 2 for R7, 3 for R10, 5 for R14.50.";
+var strAvo = "1 for R3, 2 for R7, 3 for R10, 5 for R14.50";
 
 
 
 describe('test for bestDeal Avo function', function() {
-  it('should give every total of Cape Town trippin taxi', function() {
-    var result = bestDeal(strAvo);
-    assert.equal(result, [ '1 3', '2 7', '3 10', '5 14.50' ]);
+
+  it('should give an array', function() {
+    var vee = [ '1 3', '2 7', '3 10', '5 14.50' ];
+    var result = bestDeal.Arr(strAvo);
+assert.deepEqual(result, vee);
 
 
-}
+})
+
+it("should give  array object ", function() {
+    var vee = [ '1 3', '2 7', '3 10', '5 14.50' ];
+  var vee2 = [ { quantity: '1', price: '3' },
+  { quantity: '2', price: '7' },
+  { quantity: '3', price: '10' },
+  { quantity: '5', price: '14.50' } ];
+var result = bestDeal.avoObj(vee);
+assert.deepEqual(result, vee2);
+
+
+})
+
+it("should give a price of each avo price", function(){
+  var vee2 = [ { quantity: '1', price: '3' },
+  { quantity: '2', price: '7' },
+  { quantity: '3', price: '10' },
+  { quantity: '5', price: '14.50' } ];
+var vee3 = [3, 3.5, 3.33, 2.9];
+var result = bestDeal.newArrAvo(vee2);
+assert.deepEqual(result, vee3);
+
+})
+
+
   });
