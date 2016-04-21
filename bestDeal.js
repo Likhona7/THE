cd107ca36852bc0120c6fe3ce29cb0867aa8d6dd
likhona <likhonapr@projectcodex.co>
2016-04-21 17:51:21 +0200

    latest

--- bestDeal.js
+++ bestDeal.js
@@ -38,11 +38,29 @@ avoDealArr.forEach(function(arr){
 //console.log(arr);
 var pricePerAvo = 0;
 var pricePerAvo = Number(pricePerAvo + arr.price / arr.quantity).toFixed(2);
+// console.log(arr);
+ console.log(pricePerAvo);
+
+ pricePerAvo.forEach(function(arr){
+
 console.log(arr);
-console.log(pricePerAvo);
 
 
+ })
+ console.log(arr)
 
 })
 //console.log(arr);
- }
+ // }
+ // var deals = [ { price: '3', quantity: '1' },
+ //   { price: '7', quantity: '2' },
+ //   { price: '10', quantity: '3' },
+ //   { price: '14.50', quantity: '5' } ];
+ //
+ // var deals = deals.map(function(deal){
+ //             deal.peravo = Number((deal.price/deal.quantity).toFixed(2));
+ //             return deal;
+ // });
+ //
+ // console.log(deals);
+ //
